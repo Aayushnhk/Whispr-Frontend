555411fe119a76d05ca2be46a22423db34e9721e
Aayush Sharma <139450872+Aayushnhk@users.noreply.github.com>
2025-06-28 17:00:15 +0530

    sidebar updated

--- src/app/components/chat/ChatSidebar.tsx
+++ src/app/components/chat/ChatSidebar.tsx
@@ -59,23 +59,6 @@ const ChatSidebar: React.FC<ChatSidebarProps> = ({
     ).values()
   );
 
-  const generalChatRoom: Room = {
-    _id: "general",
-    name: "General",
-    description: "General discussion for all users.",
-    createdAt: new Date(),
-    creator: {
-      _id: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      profilePicture: user.profilePicture,
-    },
-    roomPicture: "/general-chat-icon.png",
-    moderators: [],
-  };
-
-  const allRooms = [generalChatRoom, ...rooms];
-
   return (
     <div
       className={`w-64 sm:w-80 bg-gray-900 border-r border-gray-800 flex flex-col h-screen ${className}`}
@@ -95,7 +78,7 @@ const ChatSidebar: React.FC<ChatSidebarProps> = ({
       </div>
       <div className="flex-1 overflow-y-auto p-4 space-y-2">
         <ul className="space-y-2">
-          {allRooms.map((room) => (
+          {rooms.map((room) => (
             <li
               key={room._id.toString()}
               className={`p-3 rounded-lg cursor-pointer transition-colors ${
@@ -106,10 +89,6 @@ const ChatSidebar: React.FC<ChatSidebarProps> = ({
               onClick={() => {
                 handleRoomChange(room.name);
                 onSidebarItemClick();
-                if (room.name === "General") {
-                  router.push("/chat");
-                } else {
-                }
               }}
             >
               <div className="flex items-center justify-between mb-1">
@@ -144,38 +123,37 @@ const ChatSidebar: React.FC<ChatSidebarProps> = ({
                 {(user.role === "admin" ||
                   (room.creator &&
                     typeof room.creator !== "string" &&
-                    (room.creator as PopulatedCreator)._id === user.id)) &&
-                  room.name !== "General" && (
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        onEditRoomClick(room);
-                        onSidebarItemClick();
-                      }}
-                      className="ml-2 p-1.5 rounded-full text-gray-300 hover:bg-gray-700 transition-colors cursor-pointer"
-                      title="Edit Room"
+                    (room.creator as PopulatedCreator)._id === user.id)) && (
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      onEditRoomClick(room);
+                      onSidebarItemClick();
+                    }}
+                    className="ml-2 p-1.5 rounded-full text-gray-300 hover:bg-gray-700 transition-colors cursor-pointer"
+                    title="Edit Room"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-4 w-4"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="h-4 w-4"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                        />
-                      </svg>
-                    </button>
-                  )}
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                  </button>
+                )}
               </div>
             </li>
           ))}
